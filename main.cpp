8cc7f1486dfad9ec0684298093440a5cafc889d8
Reets123 <ibrahimalatig@gmail.com>
2024-09-25 04:48:47 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -16,6 +16,19 @@ public:
     // contructor
     Color() : red(0), green(0), blue(0) {}
 
-    
+    Color(int r, int g, int b) : red(r), green(g), blue(b){}
+
+    void setRed(int r) {
+        red = r;
+    }
+
+    void setGreen(int g) {
+        green = g;
+    }
+
+    void setBlue(int b){
+        blue = b;
+    }
+
 
 }
